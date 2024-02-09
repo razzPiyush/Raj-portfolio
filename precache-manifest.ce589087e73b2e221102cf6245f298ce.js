bee161d185c6deef6a08dfdc1e20350bbb6056bf
razzPiyush <irockstarpiyush@gmail.com>
2024-02-09 19:40:06 +0530

    Updates

---
+++ precache-manifest.ce589087e73b2e221102cf6245f298ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60b7ff6230e619dd6bb2",
+    "url": "/Raj-portfolio/static/css/main.edc0e518.chunk.css"
+  },
+  {
+    "revision": "60b7ff6230e619dd6bb2",
+    "url": "/Raj-portfolio/static/js/main.60b7ff62.chunk.js"
+  },
+  {
+    "revision": "39c9131ecef2fecd25da",
+    "url": "/Raj-portfolio/static/js/1.39c9131e.chunk.js"
+  },
+  {
+    "revision": "16562529cc82bdda61b8",
+    "url": "/Raj-portfolio/static/js/runtime~main.16562529.js"
+  },
+  {
+    "revision": "629dc978b32184311e13311e56954aec",
+    "url": "/Raj-portfolio/index.html"
+  }
+];
